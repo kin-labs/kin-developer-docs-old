f5c53e516fb3f9a10e3db5726141432cfd4f748e
JoshuaMaddox <joshua@cinderhousecreative.com>
2021-08-15 21:15:36 -0700

    updated site metadata

---
+++ docs/_data/site.cjs
@@ -0,0 +1,26 @@
+module.exports = function () {
+  return {
+    dir: "ltr",
+    lang: "en",
+    name: "Kin",
+    description:
+      "Kin is the easiest path for developing app experiences with cryptocurrency",
+    socialLinks: [
+      {
+        name: "GitHub",
+        url: "https://github.com/kinecosystem",
+      },
+      {
+        name: "Discord",
+        url: "https://discord.gg/kdRyUNmHDn",
+      },
+    ],
+    gitSiteUrl: "https://docs.kin.org",
+    gitBranch: "main",
+    helpUrl: "https://discord.gg/kdRyUNmHDn",
+    logoAlt: "Kin Logo",
+    iconColorMaskIcon: "#6f41e8",
+    iconColorMsapplicationTileColor: "#5728d1",
+    iconColorThemeColor: "#c9b5fd",
+  };
+};
